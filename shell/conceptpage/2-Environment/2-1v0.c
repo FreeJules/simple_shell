7ded595016281da8a50a32145d5be00b11664e5e
Elaine Yeung <yeung.egs@gmail.com>
2017-04-09 08:03:31 +0000

    Add extra exercises

--- shell/conceptpage/2-Environment/2-1v0.c
+++ shell/conceptpage/2-Environment/2-1v0.c
@@ -8,18 +8,18 @@ int main(int ac, char **av, char **environ)
 {
 	char *env_ptr;
 	char **environ_ptr;
-
 	env_ptr = getenv("SHELL");
+
+	printf("Address of env_ptr: %p\n", env_ptr);
 /*	environ_ptr =
 
 	char **pp = environ; */
 	for(environ_ptr = environ; *environ_ptr; environ_ptr++)
 	{
-		printf("%s\n", *environ_ptr);
+		;
+		/*		printf("%s\n", *environ_ptr);*/
 	}
 	printf("End of environ: %p\n", environ_ptr);
-
-	printf("Address of env_ptr: %p\n", env_ptr);
 }
 /**
  *int main(int ac, char **av, char **environ)
