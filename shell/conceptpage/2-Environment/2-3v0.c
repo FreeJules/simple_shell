898f3462a6049c0a0a344d105683619d6bf85b80
Elaine Yeung <yeung.egs@gmail.com>
2017-04-11 21:29:32 +0000

    Updated version of exercise 2-3

--- shell/conceptpage/2-Environment/2-3v0.c
+++ shell/conceptpage/2-Environment/2-3v0.c
@@ -6,7 +6,18 @@ int main(void)
 {
 
 	char* pathstring;
+	char* directory;
+	int i = 0;
 
 	pathstring =  getenv("PATH");
-	printf("%s\n",pathstring);
+
+	while ((directory = strtok(NULL, " ")) != NULL)
+	directory = strtok(pathstring, ":");
+	printf("1st directory: %s\n", directory);
+
+
+	directory = strtok(NULL, " ");
+	printf("2nd directory: %s\n", directory);
+
+	return 0;
 }
