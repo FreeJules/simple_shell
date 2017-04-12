f844992403fa5d5677e9c697ab2938c11bf9f984
Julija Lee <lee.julija@gmail.com>
2017-04-12 06:15:51 +0000

    adding prompt, getline and main

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include "shell.h"
-int main(int argc __attribute__((__unused__)), char *argv[])
+int main(int argc __attribute__((__unused__)), char *argv[] __attribute__((__unused__)))
 {
-	int i;
-
-	for(i = 1; argv[i] != NULL; i++)
-	{
-		printf("%s\n",argv[i]);
-	}
-	return 0;
+        char *buffer, *line;
+        int ret_val;
+/*
+ *      int i;
+ *      for(i = 1; argv[i] != NULL; i++)
+ *      {
+ *              printf("%s\n",argv[i]);
+ *              }
+ */
+        /* create a buffer to store input */
+        buffer = malloc(sizeof(char) * BUFF_SIZE);
+        if (buffer == NULL)
+                return (1);
+        line = NULL;
+        /* call prompt */
+        ret_val = prompt(buffer, line);
+        return (ret_val);
 }
---
+++ prompt.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+/**
+ * prompt - calls getline until user enters exit
+* @buffer: pointer to buffer to store input
+* @line: pointer to a string of line input
+* Return: 0 on success or 1 if _realloc failed
+*/
+int prompt(char *buffer, char *line)
+{
+	int chars_read, line_len, old_len, new_len;
+
+	/* call getline */
+	while (1)
+	{
+		clear_buffer(buffer);
+		chars_read = _getline(buffer, BUFF_SIZE);
+		/* need to handle if chars_read = BUFF_SIZE */
+
+		/* new_len starts from 0 and includes '\0' */
+		new_len = chars_read + 1;
+		if (line == NULL)
+			old_len = 0;
+		else
+			old_len = _strlen(line);
+		line = _realloc(line, old_len, new_len);
+		if (line == NULL)
+			return (1);
+		/* copy buffer content to new pointer of exact input size */
+		/* if input is bigger that buffer we can reallocate extra */
+		line = _memcpy(line, buffer, new_len);
+		printf("%s\n", line);
+		/* check if line = exit */
+		if (exit_shell(line))
+			break;
+		line_len = _strlen(line);
+		/* wasn't exit replace '\n' with '\0' */
+		line[line_len - 1] = '\0';
+		/* run_comand(line);*/
+	}
+	free(line);
+	free(buffer);
+	return (0);
+}
