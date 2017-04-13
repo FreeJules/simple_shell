e4ef8e38e84171aadfe217e0702a12a3374ff050
Julija Lee <lee.julija@gmail.com>
2017-04-13 23:57:53 +0000

    cleaned up main and betty checks now

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
 #include "shell.h"
-int main(int argc __attribute__((__unused__)), char *argv[] __attribute__((__unused__)))
+/**
+ * main - Entry point
+ * Return: Always 0 on succes
+ */
+int main(void)
 {
 	char *buffer, *line;
 	int ret_val;
-/*
- *      int i;
- *      for(i = 1; argv[i] != NULL; i++)
- *      {
- *              printf("%s\n",argv[i]);
- *              }
- */
 	/* create a buffer to store input */
 	buffer = malloc(sizeof(char) * BUFF_SIZE);
 	if (buffer == NULL)
