f02ecc6dd5ec6cb817b5d6ec51672805273e596f
Elaine Yeung <yeung.egs@gmail.com>
2017-04-15 23:41:51 -0700

    Remove mergediff data from main.c

--- main.c
+++ main.c
@@ -1,25 +1,4 @@
 #include "shell.h"
-<<<<<<< HEAD
-int main(int argc __attribute__((__unused__)), char *argv[] __attribute__((__unused__)))
-{
-        char *buffer, *line;
-        int ret_val;
-/*
- *      int i;
- *      for(i = 1; argv[i] != NULL; i++)
- *      {
- *              printf("%s\n",argv[i]);
- *              }
- */
-        /* create a buffer to store input */
-        buffer = malloc(sizeof(char) * BUFF_SIZE);
-        if (buffer == NULL)
-                return (1);
-        line = NULL;
-        /* call prompt */
-        ret_val = prompt(buffer, line);
-        return (ret_val);
-=======
 /**
  * main - Entry point
  * Return: Always 0 on succes
@@ -36,5 +15,4 @@ int main(void)
 	/* call cmd_line_loop */
 	ret_val = cmd_line_loop(buffer, line);
 	return (ret_val);
->>>>>>> jbranch
 }
