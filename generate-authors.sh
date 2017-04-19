27c3f8edc20b418e14939cbf1e7938c774e5db89
Elaine Yeung <yeung.egs@gmail.com>
2017-04-18 21:59:13 -0700

    organize scripts into folders

--- generate-authors.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# this script is a simplified version of docker's generate-authors script
-# https://github.com/docker/docker/blob/master/hack/generate-authors.sh
-git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf > AUTHORS
